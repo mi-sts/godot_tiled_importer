25f4c0b0642b3b5a0cb05fc5de33a31c13b1b0f1
mi-sts <66365072+mi-sts@users.noreply.github.com>
2022-03-17 23:33:23 +0300

    Plugin initialization

---
+++ addons/godot_tiled_importer/TiledImporterPlugin.cs
@@ -0,0 +1,16 @@
+#if TOOLS
+using Godot;
+using System;
+
+[Tool]
+public class TiledImporterPlugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+    }
+
+    public override void _ExitTree()
+    {
+    }
+}
+#endif
